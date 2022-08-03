c4232dece074f18dde5588e7ee3cfc9c3f2399e9
perchinkaVika <perchinka.vika@gmail.com>
2022-08-03 19:39:44 +0300

    7: created login button

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 			<main className={styles.main}>
 				<h1 className={styles.title}>
 					Welcome to <a href="https://nextjs.org">
-						Next.js!</a> integrated with{" "}
+						Next.js!</a> integrated with{" "}sss
 					<a href="https://mui.com/">Material-UI!</a>
 				</h1>
 				<p className={styles.description}>
