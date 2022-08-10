92236bf9209e7fd074cf201c99d1f58e97a44bd3
perchinkaVika <perchinka.vika@gmail.com>
2022-08-10 21:13:54 +0300

    user provider

---
+++ pages/login/index.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { Box, Button, Typography } from '@mui/material';
+import { useUserContext } from '../../src/contexts/UserContext'
+
+const Login = () => {
+    const { user, logout } = useUserContext();
+
+    useEffect(() => {
+        if (!user) {
+            google.accounts.id.renderButton(
+                document.getElementById("signInDiv"),
+                { theme: "outline", size: "large" }
+            );
+        }
+    }, [user]);
+    // If we have no user: sign in button
+    // If we have a user: show the log out button
+    return (
+        <Box width='100vw' height='100vh' alignItems='center' justifyContent='center' display='flex'>
+            <Box alignItems='center' justifyContent='center' display='flex' flexDirection='column'>
+                {!user && <Box id="signInDiv"></Box>}
+                {user &&
+                    <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center' mb={2}>
+                        {user?.picture && <img src={user?.picture}></img>}
+                        <Typography variant="h3">{user?.name}</Typography>
+                    </Box>
+                }
+
+                {user &&
+                    <Button variant='contained' onClick={logout}>Sign Out</Button>
+                }
+            </Box>
+        </Box>
+    );
+}
+export default Login;
---
+++ src/contexts/UserContext/index.jsx
@@ -0,0 +1,50 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { CircularProgress } from '@mui/material';
+import jwt_decode from "jwt-decode";
+
+const defaultValue = {
+    user: null,
+    setUser: () => {},
+    logout: () => {},
+}
+
+const UserContext = React.createContext(defaultValue);
+
+export const UserProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [allowRender, setAllowRender] = useState(false);
+
+    //////////////////////////////
+    function handleCallbackResponse(response) {
+        var userObject = jwt_decode(response.credential);
+        setUser(userObject);
+    }
+
+    useEffect(() => {
+        /*global google */
+        google.accounts.id.initialize({
+            client_id: `${process.env.NEXT_PUBLIC_CLIENT_ID}.apps.googleusercontent.com`,
+            callback: handleCallbackResponse,
+            auto_select: true,
+        });
+        setAllowRender(true);
+        google.accounts.id.prompt();
+    }, []);
+
+    if (!allowRender) {
+        return null
+    }
+
+    return <UserContext.Provider value={{
+        setUser,
+        user,
+        logout: () => {
+            setUser(null);
+            google.accounts.id.revoke();
+        },
+    }}>
+        {children}
+    </UserContext.Provider>
+}
+
+export const useUserContext = () => useContext(UserContext);
