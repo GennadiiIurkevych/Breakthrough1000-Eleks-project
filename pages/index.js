b61e52598ed9c7ca50ddd4669e9b79897f29a3f6
Gennadii Iurkevych <yurgensport@gmail.com>
2022-09-06 12:26:25 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,21 +15,3 @@ export default function Home() {
 		</>
 	);
 }
-import Header from '../src/components/Header';
-import styles from '../styles/home.module.scss';
-import styles from "../styles/Home.module.css";
-
-
-const Home = () => {
-  return (
-    <>
-      <Header keywords={Home} />
-      <div className={styles.home}>
-        
-      </div>
-
-    </>
-  ); 
-};
-
-export default Home;
